ccec0ef71f651fe8abcea3c23bee9b94fd3b11cd
Sarah Mattar <sarah.mattar@mac.com>
2021-01-08 22:30:46 -0800

    Refactor reducers

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -6,7 +6,7 @@ import {
 	SET_LOADING,
 } from '../types';
 
-export default (state, action) => {
+const reducer = (state, action) => {
 	switch (action.type) {
 		case SEARCH_USERS:
 			return {
@@ -41,3 +41,5 @@ export default (state, action) => {
 			return state;
 	}
 };
+
+export { reducer as default };
